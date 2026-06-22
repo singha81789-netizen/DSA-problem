f543d42d0f753bbdd46f9d23164afc9bb04cce73
Aryan Singh <singha81789@gmail.com>
2026-06-22 14:51:45 +0530

    Time: 13 ms (45.58%), Space: 21.2 MB (25.57%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,46 +1,46 @@
-// class Solution {
-// public:
-//  int count = 0;
-//         void expand(string s, int left, int right){
-            
-//            // while(left < right){
-//                 while(left >= 0 && right < s.size() && s[left] == s[right]){
-//                 count++;
-//                 left--;
-//                 right++;
-//             }
-//         }
-//     int countSubstrings(string s) {
-//         int n=s.size();
-        
-//         for(int i=0 ; i<s.size(); i++){
-//                 expand(s,i,i);
-//                 expand(s,i,i+1);
-
-//         }
-//         return count;
-//     }
-// };
 class Solution {
 public:
-    int count = 0;
-
-    void expand(string &s, int left, int right) {
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            count++;
-            left--;
-            right++;
+ int count = 0;
+        void expand(string s, int left, int right){
+            
+           // while(left < right){
+                while(left >= 0 && right < s.size() && s[left] == s[right]){
+                count++;
+                left--;
+                right++;
+            }
         }
-    }
-
     int countSubstrings(string s) {
-        int n = s.size();
+        int n=s.size();
+        
+        for(int i=0 ; i<s.size(); i++){
+                expand(s,i,i);
+                expand(s,i,i+1);
 
-        for (int i = 0; i < n; i++) {
-            expand(s, i, i);
-            expand(s, i, i + 1);
         }
-
         return count;
     }
 };
+// class Solution {
+// public:
+//     int count = 0;
+
+//     void expand(string &s, int left, int right) {
+//         while (left >= 0 && right < s.size() && s[left] == s[right]) {
+//             count++;
+//             left--;
+//             right++;
+//         }
+//     }
+
+//     int countSubstrings(string s) {
+//         int n = s.size();
+
+//         for (int i = 0; i < n; i++) {
+//             expand(s, i, i);
+//             expand(s, i, i + 1);
+//         }
+
+//         return count;
+//     }
+// };
