27b3f8e6f4bd0f3f796291d5af7fbfe227a18c25
Aryan Singh <singha81789@gmail.com>
2026-06-22 12:30:02 +0530

    Time: 10 ms (12.93%), Space: 22.1 MB (79.38%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    
-    bool isPal(string &s, int left, int right) {
-        while (left < right) {
-            if (s[left] != s[right])
-                return false;
+
+    bool ispalin(string &s, int left, int right){
+        while( left < right){
+        if( s[left] != s[right])
+            return false;
             left++;
             right--;
         }
         return true;
+
     }
-
     bool validPalindrome(string s) {
-        int left = 0, right = s.size() - 1;
+        int left = 0, right = s.size()-1;
 
-        while (left < right) {
-            if (s[left] == s[right]) {
+        while( left < right){
+            if(s[left] == s[right]){
                 left++;
                 right--;
-            } else {
-                return isPal(s, left + 1, right) ||
-                       isPal(s, left, right - 1);
+                
+            }else{
+            return ispalin(s,left + 1,right) || ispalin(s,left,right - 1);
+
             }
         }
-
         return true;
+        
     }
 };
