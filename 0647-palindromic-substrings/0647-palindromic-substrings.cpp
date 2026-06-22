bfe59470703318b6344f67fc04377c158d7c9745
Aryan Singh <singha81789@gmail.com>
2026-06-22 14:54:14 +0530

    Time: 2 ms (90.19%), Space: 8.4 MB (91.12%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -11,9 +11,9 @@ public:
             }
         }
     int countSubstrings(string s) {
-        int n=s.size();
+        //int n=s.size();
         
-        for(int i=0 ; i<n; i++){
+        for(int i=0 ; i<s.size(); i++){
                 expand(s,i,i);
                 expand(s,i,i+1);
 
@@ -21,26 +21,4 @@ public:
         return count;
     }
 };
-// class Solution {
-// public:
-//     int count = 0;
-
-//     void expand(string &s, int left, int right) {
-//         while (left >= 0 && right < s.size() && s[left] == s[right]) {
-//             count++;
-//             left--;
-//             right++;
-//         }
-//     }
-
-//     int countSubstrings(string s) {
-//         int n = s.size();
-
-//         for (int i = 0; i < n; i++) {
-//             expand(s, i, i);
-//             expand(s, i, i + 1);
-//         }
-
-//         return count;
-//     }
-// };
+ 
