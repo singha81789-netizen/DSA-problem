21a6af3d6c156320ed9bce3ad1ae7590bd73235d
Aryan Singh <singha81789@gmail.com>
2026-06-22 14:53:53 +0530

    Time: 3 ms (86.41%), Space: 8.5 MB (77.91%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -13,7 +13,7 @@ public:
     int countSubstrings(string s) {
         int n=s.size();
         
-        for(int i=0 ; i<s.size(); i++){
+        for(int i=0 ; i<n; i++){
                 expand(s,i,i);
                 expand(s,i,i+1);
 
