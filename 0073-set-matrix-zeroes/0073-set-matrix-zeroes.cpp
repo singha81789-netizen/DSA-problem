0eec2a4c07ec4f8342cf62268e43ba9d8368f07a
Aryan Singh <singha81789@gmail.com>
2026-07-01 13:27:45 +0530

    Time: 0 ms (100%), Space: 20.9 MB (50.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,92 +1,43 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
-    int n = matrix.size(), m = matrix[0].size();
-        bool first = false, second = false;
+          int n = matrix.size(), m = matrix[0].size();
+        bool first=false , second = false;
 
-        for(int i=0;i<n;i++)
-        if(matrix[i][0]==0) first = true;
-
-        for(int j=0;j<m;j++)
-        if(matrix[0][j]==0) second=true;
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
-                }
-            }
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0)
-                matrix[i][j]=0;
-            }
-        }
-
-        if(first)
         for(int i=0;i<n;i++){
-            matrix[i][0]=0;
+            if(matrix[i][0]==0) first= true;
         }
 
-        if(second)
-        for(int j=0;j<m;j++){
+         for(int j=0;j<m;j++){
+            if(matrix[0][j]==0) second= true;
+        }
+
+        for(int i=1;i<n;i++)
+        for(int j=1;j<m;j++)
+        if(matrix[i][j]==0){
+            matrix[i][0]=0;
             matrix[0][j]=0;
         }
+
+        for(int i=1;i<n;i++)
+        for(int j=1;j<m;j++)
+        if(matrix[i][0]==0 || matrix[0][j]==0){
+            matrix[i][j]=0;
+        }
+
+        if(first){
+            for(int i=0;i<n;i++)
+            matrix[i][0]=0;
+        }
         
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-        // int n = matrix.size(), m = matrix[0].size();
-        // bool first=false , second = false;
-
-        // for(int i=0;i<n;i++){
-        //     if(matrix[i][0]==0) first= true;
-        // }
-
-        //  for(int j=0;j<m;j++){
-        //     if(matrix[0][j]==0) second= true;
-        // }
-
-        // for(int i=1;i<n;i++)
-        // for(int j=1;j<m;j++)
-        // if(matrix[i][j]==0){
-        //     matrix[i][0]=0;
-        //     matrix[0][j]=0;
-        // }
-
-        // for(int i=1;i<n;i++)
-        // for(int j=1;j<m;j++)
-        // if(matrix[i][0]==0 || matrix[0][j]==0){
-        //     matrix[i][j]=0;
-        // }
-
-        // if(first){
-        //     for(int i=0;i<n;i++)
-        //     matrix[i][0]=0;
-        // }
-        
-        // if(second){
-        //     for(int j=0;j<m;j++)
-        //     matrix[0][j]=0;
-        // }
+        if(second){
+            for(int j=0;j<m;j++)
+            matrix[0][j]=0;
+        }
         
     
 
 
+
+    }
+};
