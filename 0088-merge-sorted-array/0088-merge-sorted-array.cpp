848b7b5fecf3d75dfdd184a6189ce3a7a102264c
Aryan Singh <singha81789@gmail.com>
2026-07-04 10:07:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (41.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,74 +1,30 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
-        int i =n-1;
-        int j=m-1;
-        int k=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+            int i = m-1;
+            int j = n-1;
+            int k = m+n-1;
+
+            while( i >= 0 && j >=0 ){
+                if(nums1[i] > nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
-            }else{
-                nums1[k]=nums2[j];
+                }else{
+                 nums1[k]=nums2[j];
+                 j--;
+            
+                }
+                k--;
+                }
+            while(j>=0){
+                nums1[k]=nums2[j];          //nums1 = [4,5,6,0,0,0]
+                                                //nums2 = [1,2,3]
                 j--;
+                k--;
             }
-            k--;
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
+
+
     }
 };
-
-// class Solution {
-// public:
-//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-//             int i = m-1;
-//             int j = n-1;
-//             int k = m+n-1;
-
-//             while( i >= 0 && j >=0 ){
-//                 if(nums1[i] > nums2[j]){
-//                 nums1[k]=nums1[i];
-//                 i--;
-//                 }else{
-//                  nums1[k]=nums2[j];
-//                  j--;
-            
-//                 }
-//                 k--;
-//                 }
-//             while(j>=0){
-//                 nums1[k]=nums2[j];          //nums1 = [4,5,6,0,0,0]
-//                                                 //nums2 = [1,2,3]
-//                 j--;
-//                 k--;
-//             }
-
-
-//     }
-// };
-
-        // int i = m-1;
-        // int j = n-1;
-        // int k = m+n-1;
-
-        // while(i >= 0 && j >= 0){
-        //     if(nums1[i] > nums2[j]){
-        //         nums1[k] = nums1[i];
-        //         i--;
-        //     } else {
-        //         nums1[k] = nums2[j];
-        //         j--;
-        //     }
-        //     k--;
-        // }
-
-        // while(j >= 0){
-        //     nums1[k] = nums2[j];
-        //     j--;
-        //     k--;
-        // }
+ 
