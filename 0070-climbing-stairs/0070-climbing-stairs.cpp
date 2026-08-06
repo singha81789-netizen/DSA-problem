91a9bbe0e320f85f675aa61b4169863cc506aff5
Aryan Singh <singha81789@gmail.com>
2026-08-06 13:06:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
 
-        int a = 1, b = 2;
+        vector<int> dp(n + 1);
 
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++)
+        {
+            int left = dp[i - 1];
+
+            int right = dp[i - 2];
+
+            dp[i] = left + right;
         }
 
-        return b;
+        return dp[n];
     }
 };
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n <= 2) return n;
+
+//         int a = 1, b = 2;
+
+//         for (int i = 3; i <= n; i++) {
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+
+//         return b;
+//     }
+// };
