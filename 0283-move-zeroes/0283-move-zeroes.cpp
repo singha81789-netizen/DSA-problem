c790f0bed2e0b7d51838cd57a70458161c0ac0e6
Aryan Singh <singha81789@gmail.com>
2026-08-11 18:33:05 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,55 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int insertpos = 0;
-
-        for(int i=0;i<nums.size();i++)
-            if(nums[i] != 0){
-                swap(nums[i] , nums[insertpos]);
-                insertpos++;
+        int j = -1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == 0){
+               j = i;
+                break;
             }
-        
+        }
+       if(j == -1)return ;
+
+
+        for(int i = j+1;i<nums.size();i++){
+            if(nums[i] != 0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+
+        }
     }
 };
 
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//        vector<int>temp;
+//         for(int i =0;i<nums.size();i++){
+//             if(nums[i] !=0){
+//             temp.push_back(nums[i]);
+//         }
+//         }
+//         int nz=temp.size();
+//         for(int i =0;i<nz;i++){
+//             nums[i]=temp[i];
+//         }
+//         for(int i=nz;i<nums.size();i++){
+//             nums[i]=0;
+//         }
+//        // return nums[i];
+  
+//     }
+// };
+
+
+
+// int insertpos = 0;
+
+//         for(int i=0;i<nums.size();i++)
+//             if(nums[i] != 0){
+//                 swap(nums[i] , nums[insertpos]);
+//                 insertpos++;
+//             }
