fba6edf1512b9184221bd7712942a42333e4ba99
Aryan Singh <singha81789@gmail.com>
2026-08-11 14:48:38 +0530

    Time: 0 ms (100%), Space: 22.7 MB (18.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-   int removeDuplicates(vector<int>& nums) {
-
-   int i = 1;   // unique element ki next position
-
-    for(int j = 1; j < nums.size(); j++) {
-
-        // naya element mila?
-        if(nums[j] != nums[i-1]) {
-            nums[i] = nums[j];   // unique jagah pe rakho
-            i++;                  // next unique position
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
+            }
         }
+        return i+1;
     }
-
-     return i;   // total unique elements
-
-}
 };
+
+
+// class Solution {
+// public:
+//    int removeDuplicates(vector<int>& nums) {
+
+//    int i = 1;   // unique element ki next position
+
+//     for(int j = 1; j < nums.size(); j++) {
+
+//         // naya element mila?
+//         if(nums[j] != nums[i-1]) {
+//             nums[i] = nums[j];   // unique jagah pe rakho
+//             i++;                  // next unique position
+//         }
+//     }
+
+//      return i;   // total unique elements
+
+// }
+// };
 
     
